619843b17bacc156285ff0929993db4236deba52
Zachary Becker <zachary.becker1@montana.edu>
2025-03-09 20:58:19 -0600

    Adding indexMap to types

--- types.h
+++ types.h
@@ -5,3 +5,15 @@ struct headerInfo {
     uint32_t height;
     uint32_t offset;
 };
+
+struct indexMap {
+    int ind_ul; // up left
+    int ind_u;  // up 
+    int ind_ur; // up right
+    int ind_l;  // left
+    int ind;    // center
+    int ind_r;  // right 
+    int ind_dl; // bottom left
+    int ind_d;  // bottom
+    int ind_dr; // bottom right
+};
