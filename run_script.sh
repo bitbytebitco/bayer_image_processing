c65d117c7fefd89b039e99f42ae5b8133eae3f8a
Zachary Becker <zachary.becker1@montana.edu>
2025-03-09 22:45:56 -0600

    updating running script

--- run_script.sh
+++ run_script.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+## change directory to src
+cd src
+
 ## Compile 
 make clean && make
 
+cd ..
+
 ## Convert PNG to BMP
-convert $1 -depth 8 -type TrueColorAlpha -define bmp:format=bmp4 -colorspace sRGB input.bmp
+convert $1 -depth 8 -type TrueColorAlpha -define bmp:format=bmp4 -colorspace sRGB output/input.bmp
 
 # Copy input image to bayer output file 
-cp input.bmp bayer.bmp 
+cp output/input.bmp output/bayer.bmp 
 
 # Run script to create Bayer Pattern (RGGB)
-./bayer_filter bayer.bmp 
+./src/bayer_filter output/bayer.bmp 
 
 # Copy bayer pattern to final output file
-cp bayer.bmp output/demosaic.bmp 
+cp output/bayer.bmp output/demosaic.bmp 
 
 # Run script to create final output
-./demosaic output/demosaic.bmp
+./src/demosaic output/demosaic.bmp
 
 xdg-open output/demosaic.bmp
