b14781368f96b2bd56612ceee81bd05baae3cd69
Zachary Becker <zachary.becker1@montana.edu>
2025-03-10 23:48:18 -0600

    changing int type

--- src/types.h
+++ src/types.h
@@ -14,15 +14,15 @@ struct headerInfo {
 };
 
 struct indexMap {
-    int ind_ul; // up left
-    int ind_u;  // up 
-    int ind_ur; // up right
-    int ind_l;  // left
-    int ind;    // center
-    int ind_r;  // right 
-    int ind_dl; // bottom left
-    int ind_d;  // bottom
-    int ind_dr; // bottom right
+    uint32_t ind_ul; // up left
+    uint32_t ind_u;  // up 
+    uint32_t ind_ur; // up right
+    uint32_t ind_l;  // left
+    uint32_t ind;    // center
+    uint32_t ind_r;  // right 
+    uint32_t ind_dl; // bottom left
+    uint32_t ind_d;  // bottom
+    uint32_t ind_dr; // bottom right
 };
 
 #endif 
