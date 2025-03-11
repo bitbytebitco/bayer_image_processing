79a77d755ddd2fec2b43a39ccae617fa0dd1ef0b
Zachary Becker <zachary.becker1@montana.edu>
2025-03-10 23:04:08 -0600

    Adding convolution kernels and organizing functions, plus commenting

--- src/demosaic.c
+++ src/demosaic.c
@@ -1,8 +1,13 @@
+/*  
+ * Filename: demosaic.c  
+ * Description: Script that runs the image interpolation from a Bayer pattern image
+ * Author: Zachary Becker  
+ */
+
 #include<stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include "utils.h"
-
 
 int main(int argc, char* argv[]) {
 
@@ -11,16 +16,17 @@ int main(int argc, char* argv[]) {
         printf("Please add an input file as the first argument (e.g. ./run_script [filename.png]");
         return 1;
     }
-    
+   
+    // user output 
     printf("\nGenerating output image using bilinear interpolation.\n");
 
+    // buffers
     char *input_fn = argv[1];
     char *buffer,*new_buf;
 
     // open with ability to read filesize
     FILE *input_image;
     input_image = fopen(input_fn,"rb");
-    
 
     // obtain filesize
     fseek(input_image, 0, SEEK_END);
@@ -34,11 +40,12 @@ int main(int argc, char* argv[]) {
     // copy input_image bytes into buffers 
     rewind(input_image);
     size_t res = fread(buffer, 1, filesize, input_image);
-    if(res !=filesize){
+    if(res != filesize){
         fprintf(stderr, "Error reading file. Expected %ld bytes, but read %ld bytes.\n", filesize, res);
         exit(1);  
     }
 
+    // copy file contents  
     rewind(input_image);
     res = fread(new_buf, 1, filesize, input_image);
     rewind(input_image);
@@ -50,121 +57,14 @@ int main(int argc, char* argv[]) {
     fclose(input_image);
     input_image=fopen(input_fn,"wb+");
     rewind(input_image);
- 
-    uint32_t bytes_per_row = hi.width * 4; // number of bytes * ( R, G, B, Padding)
-  
-    uint32_t base_index_last, base_index, base_index_next; 
-    
-    // ints for row index and column index 
-    uint32_t i,j; 
-    int k = 0; // int to keep track of column (R/G or G/B)
 
-    int row_type = 0; // 0 - RG, 1 - GB
-                
-    uint8_t B_ul, B_ur, B_dl, B_dr, G_l, G_r, G_u, G_d, B_u, B_d, R_l, R_r, R_ul, R_ur, R_dl, R_dr, B_l, B_r, R_u, R_d;
-    
-    for(i=0;i<hi.height;i++){ // for each row
-
-        k = 0; 
-
-        // calculate base indexes
-        if(i>0){
-            base_index_last = hi.offset + ((i-1) * bytes_per_row);  // previous row
-        } else {
-            base_index_last = -1;
-        }
-        
-        base_index = hi.offset + (i * bytes_per_row);  // current row 
-
-        if(i<hi.height-1){
-            base_index_next = hi.offset + ((i+1) * bytes_per_row); // next row
-        } else {
-            base_index_next = -1;
-        }
-         
-        for (j = 0; j < (bytes_per_row); j += 4) {
-
-                
-                struct indexMap im = generateIndexMap(i, j, base_index_last, base_index, base_index_next, bytes_per_row, hi.width, hi.height);
-
-
-                if(row_type == 0){ // Red/Green
-                    if(k==0){ // Red column
-
-                        // Calulate B
-                        B_ul = (unsigned char)buffer[im.ind_ul]; 
-                        B_ur = (unsigned char)buffer[im.ind_ur]; 
-                        B_dl = (unsigned char)buffer[im.ind_dl]; 
-                        B_dr = (unsigned char)buffer[im.ind_dr]; 
-                        new_buf[im.ind] = (B_ul + B_ur + B_dl + B_dr)>>2; // bitshift to divide by 4
-    
-                        // Calculate G 
-                        G_l = (unsigned char)buffer[im.ind_l+1];
-                        G_r = (unsigned char)buffer[im.ind_r+1];
-                        G_u = (unsigned char)buffer[im.ind_u+1];   
-                        G_d = (unsigned char)buffer[im.ind_d+1];   
-                        new_buf[im.ind+1] = (G_l + G_r + G_u + G_d)>>2; // bitshift to divide by 4
-
-                        k = 1;
-                    } else { // Green column
-                        // Calulate B
-                        B_u = (unsigned char)buffer[im.ind_u]; 
-                        B_d = (unsigned char)buffer[im.ind_d]; 
-                        new_buf[im.ind] = (B_u + B_d)>>1; // bitshift to divide by 2 
-
-                        // calculate R
-                        R_l = (unsigned char)buffer[im.ind_l+2];
-                        R_r = (unsigned char)buffer[im.ind_r+2];
-                        new_buf[im.ind+2] = (R_l + R_r)>>1;
-
-                        k = 0;
-                    }
-                } else { // Green/Blue
-
-                    if(k==0){ // Green Column
-                        // Calulate B
-                        B_l = (unsigned char)buffer[im.ind_l]; 
-                        B_r = (unsigned char)buffer[im.ind_r]; 
-                        new_buf[im.ind] = (B_l + B_r)>>1; // bitshift to divide by 2 
-
-                        // calculate R
-                        R_u = (unsigned char)buffer[im.ind_u+2];
-                        R_d = (unsigned char)buffer[im.ind_d+2];
-                        new_buf[im.ind+2] = (R_u + R_d)>>1;
-
-                        k = 1;
-                    } else { // Blue Column
-                        // Calculate G 
-                        G_l = (unsigned char)buffer[im.ind_l+1];
-                        G_r = (unsigned char)buffer[im.ind_r+1];
-                        G_u = (unsigned char)buffer[im.ind_u+1];   
-                        G_d = (unsigned char)buffer[im.ind_d+1];   
-                        new_buf[im.ind+1] = (G_l + G_r + G_u + G_d)>>2; // bitshift to divide by 4
-
-                        // calculate R
-                        R_ul = (unsigned char)buffer[im.ind_ul+2];
-                        R_ur = (unsigned char)buffer[im.ind_ur+2];
-                        R_dl = (unsigned char)buffer[im.ind_dr+2];
-                        R_dr = (unsigned char)buffer[im.ind_dr+2];
-                        new_buf[im.ind+2] = (R_ul + R_ur + R_dl + R_dr)>>2;
-
-                        k = 0;
-                    }
-
-                }
-
-        }
-
-
-    // toggle row type
-    row_type ^= 1;
-
-    }
-    
+    // run an arbitrary interpolation algorithm
+    runBilinearInterpolation(buffer, new_buf, hi); 
 
     // overwrite input file with buffer contents
     write_output_to_file(input_image, filesize, new_buf); 
 
+    // clean up
     fclose(input_image);
     free(buffer);
     free(new_buf);
--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,28 @@
+/*  
+ * Filename: utils.c  
+ * Description: Utility functions for Bayer pattern generation and interpolation 
+ * Author: Zachary Becker  
+ */
+
 #include<stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include "types.h"
 
+// Convolution Kernels (arbitrary algorithm kernels)
+float greenKernel[3][3] = {
+    {0, 0.25, 0},
+    {0.25, 0, 0.25},
+    {0, 0.25, 0}
+};
 
+float redBlueKernel[3][3] = {
+    {0.25, 0, 0.25},
+    {0, 0, 0},
+    {0.25, 0, 0.25}
+};
+
+// Parses BMP header for width, height, data offset
 struct headerInfo get_header_info(FILE *input_image){
     int32_t h;
     uint32_t width;
@@ -12,9 +31,11 @@ struct headerInfo get_header_info(FILE *input_image){
 
     struct headerInfo hi;
 
+    // parse offset
     fseek(input_image, 10, SEEK_SET);
     size_t res = fread(&offset, sizeof(offset), 1, input_image); 
 
+    // parse image width
     fseek(input_image, 18, SEEK_SET);
     res = fread(&width, sizeof(width), 1, input_image); 
     if(res != 1){
@@ -22,6 +43,7 @@ struct headerInfo get_header_info(FILE *input_image){
         exit(1);  
     }
 
+    // parse image height 
     fseek(input_image, 22, SEEK_SET);
     res = fread(&h, sizeof(h), 1, input_image); 
     if(res != 1){
@@ -30,14 +52,15 @@ struct headerInfo get_header_info(FILE *input_image){
     }
     rewind(input_image);
 
+    // using two's complement to obtain positive value
     if(h && (1 << 31)){
         height = ~h+1;
         height = ~height+1;
-        //height = height * -1;
     } else {
         height = h;
     }
 
+    // filling output struct
     hi.width = width;
     hi.height = height;
     hi.offset = offset;
@@ -45,22 +68,30 @@ struct headerInfo get_header_info(FILE *input_image){
     return hi;
 }
 
-struct indexMap generateIndexMap(int i, int j, int base_index_last, int base_index, int base_index_next, int bytes_per_row, int width, int height){
+// generates a map of the byte count indexes needed to generate a 3x3 matrix of a section of the image (handles mirroring)
+struct indexMap generateIndexMap(int i, int j, int base_index_last, int base_index, int base_index_next, int bytes_per_row, int height){
 
     struct indexMap im;
 
+    // 3x3 matrix of indexes
+    // ul, u, ur
+    // l, ind, r
+    // dl, d, dr 
+
+    // top row
     im.ind_ul = base_index_last + j - 4; 
     im.ind_u = base_index_last + j; 
     im.ind_ur = base_index_last + j + 4; 
-    
+   
+    // middle row 
     im.ind_l = base_index + j - 4; 
     im.ind = base_index + j; 
     im.ind_r = base_index + j + 4; 
 
+    // bottom row
     im.ind_dl = base_index_next + j - 4; 
     im.ind_d = base_index_next + j; 
     im.ind_dr =base_index_next + j + 4; 
-
 
     // mirroring values
     if(j == 0){ // if on the first column 
@@ -88,7 +119,6 @@ struct indexMap generateIndexMap(int i, int j, int base_index_last, int base_ind
 
         } 
         
-    } else if(j == (width-1)){ // if last column
     } else if(j >= bytes_per_row - 4){ // if last column
 
         if(i == 0){ // if first row (top right corner)
@@ -116,10 +146,13 @@ struct indexMap generateIndexMap(int i, int j, int base_index_last, int base_ind
         }
 
     } else { // if middle columns 
+
         if(i == 0){ // first row
+
             im.ind_ul = im.ind_dl;
             im.ind_u = im.ind_d;
             im.ind_ur = im.ind_dr;
+
         } else if(i == (height-1)) { // last row
 
             im.ind_dl = im.ind_ul;
@@ -127,12 +160,149 @@ struct indexMap generateIndexMap(int i, int j, int base_index_last, int base_ind
             im.ind_dl = im.ind_ur;
 
         }
+            
     }
 
     return im;
 
 }
 
+// Applies a 3x3 convolution kernel to a 3x3 image value matrix
+int applyKernel(int image[3][3], float kernel[3][3]) {
+    int result = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            result += image[i][j] * kernel[i][j];
+        }
+    }
+
+    return result;
+}
+
+// Arbitrary interpolation algorithm
+void runBilinearInterpolation(char * buffer, char * new_buf, struct headerInfo hi){
+
+    // number of bytes * ( R, G, B, Padding)
+    uint32_t bytes_per_row = hi.width * 4; 
+
+    // buffer index value for the first byte in a row  
+    uint32_t base_index_last, base_index, base_index_next; 
+    
+    // ints for row index and column index 
+    uint32_t i,j; 
+
+    // int to keep track of column (0 - R/G or 1 - G/B)
+    int k;
+
+     // row type (0 - RG, 1 - GB)
+    int row_type = 1;
+   
+    // vars to make simple averaging operations easier to read             
+    uint8_t B_u, B_d, R_l, R_r, B_l, B_r, R_u, R_d;
+    
+    for(i=0;i<hi.height;i++){ // for each row
+
+        // set column type to R/G
+        k = 0; 
+
+        // calculate base indexes
+        // previous row
+        if(i>0){
+            base_index_last = hi.offset + ((i-1) * bytes_per_row);  
+        } else {
+            base_index_last = -1;
+        }
+
+        // current row 
+        base_index = hi.offset + (i * bytes_per_row);  
+
+        // next row
+        if(i<hi.height-1){
+            base_index_next = hi.offset + ((i+1) * bytes_per_row); 
+        } else {
+            base_index_next = -1;
+        }
+        
+        // iterate through bytes (skipping every fourth, which is padding) 
+        for (j = 0; j < (bytes_per_row); j += 4) {
+
+                // generate 3x3 buffer index map
+                struct indexMap im = generateIndexMap(i, j, base_index_last, base_index, base_index_next, bytes_per_row, hi.height);
+
+                if(row_type == 0){ // Red/Green
+                    if(k==0){ // Red column
+
+                        // generate image matrix
+                        int image[3][3] = {    
+                            {(unsigned char)buffer[im.ind_ul], (unsigned char)buffer[im.ind_u+1] , (unsigned char)buffer[im.ind_ur]},
+                            {(unsigned char)buffer[im.ind_l+1], 0 , (unsigned char)buffer[im.ind_r+1]},
+                            {(unsigned char)buffer[im.ind_dl], (unsigned char)buffer[im.ind_d+1] , (unsigned char)buffer[im.ind_dr]}
+                        };
+
+                        // Calulate B
+                        new_buf[im.ind] = applyKernel(image, redBlueKernel);
+   
+                        // Calculate G 
+                        new_buf[im.ind+1] = applyKernel(image, greenKernel);
+
+                        k = 1;
+                    } else { // Green column
+                        // Calulate B
+                        B_u = (unsigned char)buffer[im.ind_u]; 
+                        B_d = (unsigned char)buffer[im.ind_d]; 
+                        new_buf[im.ind] = (B_u + B_d)>>1; // bitshift to divide by 2 
+
+                        // calculate R
+                        R_l = (unsigned char)buffer[im.ind_l+2];
+                        R_r = (unsigned char)buffer[im.ind_r+2];
+                        new_buf[im.ind+2] = (R_l + R_r)>>1; // bitshift to divide by 2 
+
+                        k = 0;
+                    }
+                } else { // Green/Blue
+
+                    if(k==0){ // Green Column
+                        // Calulate B
+                        B_l = (unsigned char)buffer[im.ind_l]; 
+                        B_r = (unsigned char)buffer[im.ind_r]; 
+                        new_buf[im.ind] = (B_l + B_r)>>1; // bitshift to divide by 2 
+
+                        // calculate R
+                        R_u = (unsigned char)buffer[im.ind_u+2];
+                        R_d = (unsigned char)buffer[im.ind_d+2];
+                        new_buf[im.ind+2] = (R_u + R_d)>>1; // bitshift to divide by 2 
+
+                        k = 1;
+                    } else { // Blue Column
+
+                        // generate image matrix
+                        int image[3][3] = {    
+                            {(unsigned char)buffer[im.ind_ul+2], (unsigned char)buffer[im.ind_u+1] , (unsigned char)buffer[im.ind_ur+2]},
+                            {(unsigned char)buffer[im.ind_l+1], 0 , (unsigned char)buffer[im.ind_r+1]},
+                            {(unsigned char)buffer[im.ind_dl+2], (unsigned char)buffer[im.ind_d+1] , (unsigned char)buffer[im.ind_dr+2]}
+                        };
+
+                        // Calculate G 
+                        new_buf[im.ind+1] = applyKernel(image, greenKernel);
+
+                        // calculate R
+                        new_buf[im.ind+2] = applyKernel(image, redBlueKernel);
+
+                        k = 0;
+                    }
+
+                }
+
+        }
+
+    // toggle row type
+    row_type ^= 1;
+
+    }
+}
+
+// write buffer to output file
 void write_output_to_file(FILE *input_image, long filesize, char *buffer){
 
     // go to beginning of data    
@@ -148,7 +318,6 @@ void write_output_to_file(FILE *input_image, long filesize, char *buffer){
     printf("total_chunks: %lu\n", total_chunks);
 
     for (size_t m = 0; m < total_chunks; m++) {
-
         bytes_written = fwrite(buffer + (m * chunk_size), 1, chunk_size, input_image);
         if (bytes_written != chunk_size) {
             perror("Error writing to file");
